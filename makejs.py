7ba9b420859ca81d10a9d1797e45e40d9b8e9007
wuyi1214 <119146376+wuyi1214@users.noreply.github.com>
2024-12-23 14:25:06 +0800

    完成遍历

--- makejs.py
+++ makejs.py
@@ -169,7 +169,7 @@ str_3='''        // ...（30个对象的数组，每个对象包含标题和5个
 def longstr(list_of_it):
     fstr = f"        {{ title: '{list_of_it[0]}', images: ['{list_of_it[1]}', '{list_of_it[2]}', '{list_of_it[3]}', '{list_of_it[4]}', '{list_of_it[5]}'] }},"
     return fstr
-list_list_of_it = [['无画风原图','og/1.png','og/2.png','og/3.png','null.png','null.png']]
+list_list_of_it = [['无画风原图','og/1.png','og/2.png','og/3.png','og/4.png','og/5.png']]
 with open('artist.txt','r') as f:
     while True:
         st = f.readline().strip()
